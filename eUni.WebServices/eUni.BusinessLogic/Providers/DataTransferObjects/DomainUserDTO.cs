b15bc44adb6769297496a1bb6e784e29556aa59e
razvansalajan <razvan_dan94@yahoo.com>
2015-11-03 20:35:44 +0200

    providers for getUsers use-case.

---
+++ eUni.WebServices/eUni.BusinessLogic/Providers/DataTransferObjects/DomainUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUni.BusinessLogic.Providers.DataTransferObjects
+{
+    public class DomainUserDTO
+    {
+        public int DomainUserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MatriculationNumber { get; set; }
+    }
+}
---
+++ eUni.WebServices/eUni.BusinessLogic/Providers/UserProvider.cs
@@ -0,0 +1,35 @@
+﻿using eUni.BusinessLogic.Providers.DataTransferObjects;
+using eUni.DataAccess.Domain;
+using eUni.DataAccess.eUniDbContext;
+using eUni.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUni.BusinessLogic.Providers
+{
+    public class UserProvider
+    {
+        private IUserRepository userRepo = new UserRepository(new ApplicationDbContext()); 
+
+        public List<DomainUserDTO> GetAllUsers()
+        {
+            IEnumerable<DomainUser> enumerableAllUsers = userRepo.GetAll();
+            List<DomainUserDTO> allUsers = new List<DomainUserDTO>();
+            foreach(var currentUser in enumerableAllUsers)
+            {
+                allUsers.Add(new DomainUserDTO()
+                {
+                    DomainUserId = currentUser.DomainUserId,
+                    Email = currentUser.Email,
+                    FirstName = currentUser.FirstName,
+                    LastName = currentUser.LastName,
+                    MatriculationNumber = currentUser.MatriculationNumber
+                });
+            }
+            return allUsers;
+        }
+    }
+}
