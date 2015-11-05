e6170eee6265ee9b2393b336a584fd4a1f0c4491
catalincalina <catalinf.calina@gmail.com>
2015-11-05 20:03:21 +0200

    Implemented GetAllUsers() functionality

--- eUni.WebServices/eUni.WebServices/App_Start/AutoMapperConfig.cs
+++ eUni.WebServices/eUni.WebServices/App_Start/AutoMapperConfig.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AutoMapper;
 using eUni.BusinessLogic.AutoMapper;
+using eUni.BusinessLogic.Providers.DataTransferObjects;
+using eUni.WebServices.Models;
 
 namespace eUni.WebServices
 {
@@ -11,6 +14,17 @@ namespace eUni.WebServices
         public static void Configure()
         {
             BusinessLogicMapper.UserMappings();
+            UserDTOToViewModel();
+        }
+
+        public static void UserDTOToViewModel()
+        {
+            Mapper.CreateMap<DomainUserDTO, UserViewModel> ()
+                .ForMember(dest => dest.DomainUserId, opt => opt.MapFrom(src => src.DomainUserId))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.MatriculationNumber, opt => opt.MapFrom(src => src.MatriculationNumber));
         }
     }
 }
--- eUni.WebServices/eUni.WebServices/Controllers/UserController.cs
+++ eUni.WebServices/eUni.WebServices/Controllers/UserController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using AutoMapper;
+using eUni.BusinessLogic.Providers;
+using eUni.BusinessLogic.Providers.DataTransferObjects;
+using eUni.WebServices.Models;
 
 namespace eUni.WebServices.Controllers
 {
+    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
+        private UserProvider userProvider = new UserProvider();
+
         public string Get(int id)
         {
             return "value";
+        }
+
+        [Route("AllUsers")]
+        public async Task<IHttpActionResult> GetAllUsers()
+        {
+            List<DomainUserDTO> users = userProvider.GetAllUsers();
+            var allUsers = Mapper.Map<IEnumerable<UserViewModel>>(users);
+
+            return Content(HttpStatusCode.OK, allUsers);
         }
     }
 }
