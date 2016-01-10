6c934f468eca8d48cbe00b74529476120cb40dca
Adrian Achim <adrian.achim94@gmail.com>
2016-01-10 23:32:12 +0200

    dadada dadada dadaad dadada

--- eUni.Client/eUni.Client/Services/Extensions.cs
+++ eUni.Client/eUni.Client/Services/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.IO;
+using System.Web;
 
 namespace EUni_Client.Services
 {
@@ -7,6 +8,20 @@ namespace EUni_Client.Services
         public static ApiService GetApiService(this HttpSessionStateBase session)
         {
             return session[ServiceNames.ApiService] as ApiService;
+        }
+
+        public static byte[] ToByteArray(this Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
         }
     }
 }
