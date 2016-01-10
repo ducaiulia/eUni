b133324e117a370c0370a759eabeb9743efa7ea6
Mircea <mirceatoda@gmail.com>
2016-01-10 18:51:40 +0200

    course went into wiki page

--- eUni.Client/eUni.Client/Controllers/WikiController.cs
+++ eUni.Client/eUni.Client/Controllers/WikiController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace EUni_Client.Controllers
 {
     public class WikiController : Controller
     {
         // GET: Wiki
-        public ActionResult Index()
+        public ActionResult Index(string Course)
         {
+            if (Course != null)
+                ViewBag.Course = JsonConvert.DeserializeObject(Course);
             return View();
         }
 
