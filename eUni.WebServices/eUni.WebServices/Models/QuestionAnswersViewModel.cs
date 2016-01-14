0f3c5a3e84c4f57e8af96814ca95a451b89b99bf
iuliaduca <iulianaboca94@yahoo.com>
2016-01-14 21:58:13 +0200

    i forgot this :D

---
+++ eUni.WebServices/eUni.WebServices/Models/QuestionAnswersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUni.WebServices.Models
+{
+    public class QuestionAnswersViewModel
+    {
+        public int QuestionId { get; set; }
+        public IEnumerable<AnswerViewModel> Answers { get; set; }
+    }
+}
